bb24be66bfae10a1bf8d62fdf18e07a52e05ade1
mvogt <mike.vogt@iee.fraunhofer.de>
2022-09-30 22:25:39 +0200

    first working version which can export qgis layer to a pandapower network

--- pandapower_qgis.py
+++ pandapower_qgis.py
@@ -23,12 +23,18 @@
 """
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import QAction, QFileDialog, QProgressBar
+from qgis.core import QgsProject, QgsWkbTypes, QgsMessageLog, Qgis, QgsDistanceArea, QgsPointXY
 
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
-from .pandapower_qgis_dialog import ppqgisDialog
+from .pandapower_import_dialog import ppImportDialog
+from .pandapower_export_dialog import ppExportDialog
+
+# install requirements
+import sys
+import pathlib
 import os.path
 
 
@@ -65,7 +71,41 @@ class ppqgis:
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
-        self.first_start = None
+        self.first_start_import = None
+        self.first_start_export = None
+        self.dlg_import = None
+        self.dlg_export = None
+        self.dir = None
+        self.layer_id_dict = None
+
+    def installer_func(self):
+        plugin_dir = os.path.dirname(os.path.realpath(__file__))
+
+        try:
+            import pip
+        except ImportError:
+            QgsMessageLog.logMessage("pip missing, trying to install/update pip.",
+                                     level=Qgis.MessageLevel.Info)
+            exec(open(str(pathlib.Path(plugin_dir, 'scripts', 'get_pip.py'))).read())
+            import pip
+            import subprocess
+            # just in case the included version is old
+            # pip.main(['install', '--upgrade', 'pip'])
+            subprocess.check_call(["pip", "install", "--upgrade", "pip"])
+
+        sys.path.append(plugin_dir)
+
+        with open(os.path.join(plugin_dir, 'requirements.txt'), "r") as requirements:
+            for dep in requirements.readlines():
+                dep = dep.strip().split("==")[0]
+                try:
+                    __import__(dep)
+                    QgsMessageLog.logMessage("Trying to load {}".format(dep), level=Qgis.MessageLevel.Info)
+                except ImportError as e:
+                    import subprocess
+                    QgsMessageLog.logMessage("{} not available, installing".format(dep),
+                                             level=Qgis.MessageLevel.Warning)
+                    subprocess.check_call(["pip", "install", dep])
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -81,7 +121,6 @@ class ppqgis:
         """
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('ppqgis', message)
-
 
     def add_action(
         self,
@@ -163,13 +202,19 @@ class ppqgis:
         icon_path = ':/plugins/pandapower_qgis/icon.png'
         self.add_action(
             icon_path,
-            text=self.tr(u'pandapower Networks'),
-            callback=self.run,
+            text=self.tr(u'export to pandapower'),
+            callback=self.exprt,
+            parent=self.iface.mainWindow())
+
+        self.add_action(
+            icon_path,
+            text=self.tr(u'import from pandapower'),
+            callback=self.imprt,
             parent=self.iface.mainWindow())
 
         # will be set False in run()
-        self.first_start = True
-
+        self.first_start_import = True
+        self.first_start_export = True
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -179,22 +224,155 @@ class ppqgis:
                 action)
             self.iface.removeToolBarIcon(action)
 
-
-    def run(self):
+    def exprt(self):
         """Run method that performs all the real work"""
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if self.first_start == True:
-            self.first_start = False
-            self.dlg = ppqgisDialog()
+        if self.first_start_export:
+            self.first_start_export = False
+            self.dlg_export = ppExportDialog()
+
+        # get all layers
+        layers = QgsProject.instance().mapLayers()
+        current_crs = QgsProject.instance().crs().authid()
+
+        # clear both comboboxes
+        self.dlg_export.busComboBox.clear()
+        self.dlg_export.lineComboBox.clear()
+
+        # iterate through all layers and set up a reverse lookup table
+        self.layer_id_dict = {"-": None}
+        self.dlg_export.busComboBox.addItem("-")
+        self.dlg_export.lineComboBox.addItem("-")
+
+        for layer_id, layer in layers.items():
+            self.dlg_export.busComboBox.addItem(layer.name())
+            self.dlg_export.lineComboBox.addItem(layer.name())
+            self.layer_id_dict[layer.name()] = layer_id
 
         # show the dialog
-        self.dlg.show()
+        self.dlg_export.show()
         # Run the dialog event loop
-        result = self.dlg.exec_()
+        result = self.dlg_export.exec_()
         # See if OK was pressed
         if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+            self.installer_func()
+
+            import pandapower as pp
+
+            net = pp.create_empty_network()
+
+            bus_layer_id = self.layer_id_dict[self.dlg_export.busComboBox.currentText()]
+            line_layer_id = self.layer_id_dict[self.dlg_export.lineComboBox.currentText()]
+            vn_kv = float(self.dlg_export.vnKvTextEdit.toPlainText())
+            stdType = self.dlg_export.stdTypeTextEdit.toPlainText()
+
+            bus_lookup = dict()
+
+            if bus_layer_id:
+                layer = layers[bus_layer_id]
+                features = layer.getFeatures()
+                for feature in features:
+                    geom = feature.geometry()
+                    geomSingleType = QgsWkbTypes.isSingleType(geom.wkbType())
+                    if geom.type() == QgsWkbTypes.GeometryType.PointGeometry:
+                        if geomSingleType:
+                            x = geom.asPoint()
+                            # QgsMessageLog.logMessage("Point: X: " + str(x.x()) + ", Y: " + str(x.y()),
+                            #                         level=Qgis.MessageLevel.Info)
+                            id = pp.create_bus(net, geodata=(x.x(), x.y()), vn_kv=vn_kv)
+                            bus_lookup[x] = id
+
+            if line_layer_id:
+                layer = layers[line_layer_id]
+                features = layer.getFeatures()
+                for feature in features:
+                    geom = feature.geometry()
+                    geomSingleType = QgsWkbTypes.isSingleType(geom.wkbType())
+                    if geom.type() == QgsWkbTypes.GeometryType.LineGeometry:
+                        if geomSingleType:
+                            x = geom.asPolyline()
+                            QgsMessageLog.logMessage("Line: " + str(x), level=Qgis.MessageLevel.Info)
+                        else:
+                            # x = geom.asMultiPolyline()
+                            # x = geom.asPolyline()
+
+                            d = QgsDistanceArea()
+                            d.setEllipsoid(current_crs)
+                            for part in geom.parts():
+                                # for point in part.points():
+                                #     res += "[" + str(point.x()) + ", " + str(point.y()) + "]"
+                                frst = part.points()[0]
+                                last = part.points()[-1]
+                                distance_first = sys.float_info.max
+                                distance_last = sys.float_info.max
+                                bus_found_first = -1
+                                bus_found_last = -1
+                                for bus in bus_lookup.keys():
+                                    m = d.measureLine([QgsPointXY(frst), bus])
+                                    if m < distance_first:
+                                        distance_first = m
+                                        bus_found_first = bus_lookup[bus]
+
+                                    m = d.measureLine([QgsPointXY(last), bus])
+
+                                    if m < distance_last:
+                                        distance_last = m
+                                        bus_found_last = bus_lookup[bus]
+
+                                if bus_found_first != 1 and bus_found_last != 1:
+                                    # length = d.measureLine(QgsPointXY(part.points()))
+                                    geo = []
+                                    for point in part.points():
+                                        geo.append([point.x(), point.y()])
+                                    pp.create_line(net,
+                                                   from_bus=bus_found_first,
+                                                   to_bus=bus_found_last,
+                                                   std_type=stdType,
+                                                   length_km=part.length()/1000.,
+                                                   geodata=geo)
+                                    #QgsMessageLog.logMessage("Line from {0} to {1}".format(bus_found_first, bus_found_last),
+                                    #                         level=Qgis.MessageLevel.Info)
+
+            filters = "pandapower networks (*.json)"
+            selected = "pandapower networks (*.json)"
+            file = QFileDialog.getSaveFileName(None, "File Dialog", self.dir, filters, selected)[0]
+            if file:
+                pp.to_json(net, file)
+
+    def imprt(self):
+        """Run method that performs all the real work"""
+
+        # Create the dialog with elements (after translation) and keep reference
+        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
+        if self.first_start_import:
+            self.first_start_import = False
+            self.dlg_import = ppImportDialog()
+
+        dlg = QFileDialog()
+        # dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setNameFilter("pandapower networks (*.json)")
+        dlg.selectNameFilter("pandapower networks (*.json)")
+
+        filters = "pandapower networks (*.json)"
+        selected = "pandapower networks (*.json)"
+        file = QFileDialog.getOpenFileName(None, "File Dialog", self.dir, filters, selected)[0]
+
+        if file:
+            self.installer_func()
+            import pandapower as pp
+            import pandapower.networks as networks
+            # net = pp.from_json(file)
+            net = networks.mv_oberrhein()
+            self.dlg_import.BusLabel.setText("#Bus: " + str(len(net.bus)))
+            self.dlg_import.LineLabel.setText("#Lines: " + str(len(net.line)))
+            # show the dialog
+            self.dlg_import.show()
+            # Run the dialog event loop
+            result = self.dlg_import.exec_()
+            # See if OK was pressed
+            if result:
+                # Do something useful here - delete the line containing pass and
+                # substitute with your code.
+                pass
