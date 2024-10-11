cab88ab30f0e5ef328b551f171f30ded2758a51b
slee <sodi226@gmail.com>
2024-10-11 16:30:34 +0200

    fixed some bugs to make it usable with pp 3.0.0

--- geo.py
+++ geo.py
@@ -125,7 +125,7 @@ def dump_to_geojson(net, nodes=False, branches=False):
         if isinstance(nodes, bool):
             iterator = node_geodata.iterrows()
         else:
-            iterator = node_geodata.loc[nodes].iterrows()
+            iterator = node_geodata.loc[list(nodes)].iterrows()
         for uid, row in iterator:
             if is_pandapower and row.coords is not None and not pd.isna(row.coords):
                 # [(x, y), (x2, y2)] start and end of bus bar
@@ -164,7 +164,7 @@ def dump_to_geojson(net, nodes=False, branches=False):
             # if all iterating over pipe
             iterator = net.line_geodata.iterrows() if is_pandapower else net.pipe.iterrows()
         else:
-            iterator = net.line_geodata.loc[branches].iterrows() if is_pandapower else net.pipe.loc[branches].iterrows()
+            iterator = net.line_geodata.loc[list(branches)].iterrows() if is_pandapower else net.pipe.loc[list(branches)].iterrows()
         for uid, row in iterator:
             if not is_pandapower:
                 coords = []
